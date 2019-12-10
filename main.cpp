d8f23d5fad84dbfc561306d605e9b6087d2419af
Digya Mandala Putra <digyamp@gmail.com>
2019-12-10 10:09:05 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+  CALCULATE MINIMUM NUMBER OF COINS NEEDED TO CHANGE [1,2,3,4,5...]
+  USING DYNAMIC PROGRAMMING
+
+  Formula to find minimum coins to change 'x':
+
+  f(x) = min( f(x-coin1)+1,
+              f(x-coin2)+1,
+              f(x-coin3)+1,
+              f(x-coin4)+1,
+              ...,
+              ...
+            )
+*/
+
+void setToZeroes(int* x) {
+  for(int i = 0 ; i < 100; i++)
+    x[i] = 0;
+}
+
+int min(int a, int b){
+  if(a < b)
+    return a;
+  return b;
+}
+
+vector<int> generateSolveTable(int* coin, int coin_length, int coin_to_change) {
+
+  int MAX = 99999;
+  vector<int> solve(1000);
+  solve[0] = 0; //base case
+  
+  for(int i = 1 ; i <= coin_to_change ; i++) {
+    solve[i] = MAX;
+    for(int j = 0; j < coin_length ; j++){
+      if(i - coin[j] < 0){
+        //assuming the value of coins are sorted
+        break;
+      }
+      solve[i] = min(solve[i], solve[i - coin[j]] + 1);
+    }
+  }
+  return solve;
+}
+
+int main() {
+
+  int coins[100];
+  setToZeroes(coins);
+  coins[0] = 1;
+  coins[1] = 3;
+  coins[2] = 4;
+  coins[3] = 15;
+  int lenCoin = 4;
+
+  int x;
+  cin >> x;
+
+  vector<int> numberOfCoinsNeeded = generateSolveTable(coins, lenCoin, x);
+
+  for(int i = 0 ; i <= x ; i++){
+    cout << "max change for " << i << ": " << numberOfCoinsNeeded[i] << endl;
+  }
+
+  return 0;
+}
